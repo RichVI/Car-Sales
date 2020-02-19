7db31d95f9d28641949a9cca2e81c272995e7f47
richvi <richardwang2050@gmail.com>
2020-02-19 00:45:38 -0800

    MVP complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+// Step 1b - import createStore & defaultReducer and create createStore
+// Step 1c - import and use Provider and pass Store props to <App> in index.js 
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { carReducer } from './reducers/carReducer';
+import { Provider } from 'react-redux';
 import App from './App';
-
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+
+const store = createStore(carReducer);
+console.log ("App-Store", store.getState());
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
